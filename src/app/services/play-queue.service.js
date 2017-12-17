5aaccbd6d288acce4616b4481e2c5ec7cf465930
Philipp Keck <git@philippkeck.de>
2017-12-17 17:43:55 +0100

    Revert "remove consume button"
    This reverts commit d57d73fe69b2e7238754620a3a682c7a684c249c.

--- src/app/services/play-queue.service.js
+++ src/app/services/play-queue.service.js
@@ -87,6 +87,11 @@ class PlayQueueService {
     this.socketService.emit('removeFromQueue', {value: index});
   }
 
+  toggleConsume() {
+    this.$log.debug('setConsume', !this.playerService.state.consume);
+    this.socketService.emit('setConsume', {value: !this.playerService.state.consume});
+  }
+
   clearQueue() {
     this.$log.debug('clearQueue');
     this.socketService.emit('clearQueue');
